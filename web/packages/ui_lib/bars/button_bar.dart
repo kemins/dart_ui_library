3b661455b1ab2f50319904f8229aa32c6a4fc34a
Andrew <andriy.kemin@kemin.gmail.com>
2014-04-15 14:31:30 +0300

    Button bar - add support of button classes (selected/unselected)

--- web/packages/ui_lib/bars/button_bar.dart
+++ web/packages/ui_lib/bars/button_bar.dart
@@ -9,6 +9,15 @@ class ButtonBar extends PolymerElement{
   
   String get lastClickedValue => _lastClickedValue;
   
+  @published
+  String buttonClass = "button_bar_button";
+  
+  @published
+  String selectedButtonClass = "button_bar_selected_button";
+  
+  @override
+  bool get applyAuthorStyles => true;
+  
   void set dataProvider(List value) {
     
     var oldValue = _dataProvider;
@@ -22,8 +31,36 @@ class ButtonBar extends PolymerElement{
   }
   
   void onButtonClick(MouseEvent event, detail, target) {
-    _lastClickedValue = (target as ButtonElement).value;
+    
+    ButtonElement selectedButton = (target as ButtonElement);
+    _lastClickedValue = selectedButton.value;
+    
+    _applyClassForButtons(selectedButton);
     dispatchEvent(new Event("button-click", canBubble: true, cancelable: true));
+  }
+  
+  
+  void _applyClassForButtons(ButtonElement selectedButton) {
+    
+    ElementList buttons = shadowRoot.querySelectorAll("button");
+    List<String> buttonClasses = buttonClass.split(" ");
+    List<String> buttonSelectedClasses = selectedButtonClass.split(" ");
+    
+    void applyClass(ButtonElement btn) {
+      btn.classes.clear();
+      
+      if (btn == selectedButton) {
+        buttonClasses.forEach((String claz) => btn.classes.remove(claz));
+        buttonSelectedClasses.forEach((String claz) => btn.classes.add(claz));
+      }else {
+        buttonSelectedClasses.forEach((String claz) => btn.classes.remove(claz));
+        buttonClasses.forEach((String claz) => btn.classes.add(claz));
+      }
+    }
+    
+    
+    buttons.forEach(applyClass);
+    
   }
   
   List _dataProvider = toObservable([]);
